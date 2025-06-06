c70568067b31a62395bbc958e717f30255850051
Tharun <er.tharunravi@gmail.com>
2025-06-06 10:49:53 +0530

    Push singleton design pattern

---
+++ SIngleton.Pattern/Models/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIngleton.Pattern.Models
+{
+    public class Singleton
+    {
+        private static Singleton? Instance;
+        private readonly static object _lock = new object();
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(Instance == null)
+            {
+                lock(_lock)
+                {
+                    if (Instance is null)
+                        Instance = new Singleton();
+                }
+            }
+            return Instance;
+        }
+    }
+}
---
+++ SIngleton.Pattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using SIngleton.Pattern.Models;
+
+namespace SIngleton.Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton firstObj = Singleton.GetInstance();
+            Singleton secondObj = Singleton.GetInstance();
+            Singleton thirdObj = Singleton.GetInstance();
+            Singleton fourthObj = Singleton.GetInstance();
+            Singleton fifthObj = Singleton.GetInstance();
+
+            var obj1 = Task.Run(Singleton.GetInstance).Result;
+            var obj2 = Task.Run(Singleton.GetInstance).Result;
+            var obj3 = Task.Run(Singleton.GetInstance).Result;
+
+            if(obj1 == obj2 && obj2 == obj3 && obj3 == obj1)
+                Console.WriteLine("Multithreaded Singleton objects created successfully");
+            else
+                Console.WriteLine("Failed to create Multithreaded singleton objects");
+            if (firstObj == secondObj && secondObj == thirdObj && thirdObj == fourthObj && fourthObj ==fifthObj && fifthObj == firstObj)
+                Console.WriteLine("Singleton objects created successfully");
+            else
+                Console.WriteLine("Failed to create singleton objects");
+        }
+    }
+}
