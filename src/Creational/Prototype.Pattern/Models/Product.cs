277d836326bd3cdaa021449fccbe61bb71887ae7
Tharun <er.tharunravi@gmail.com>
2025-06-07 09:03:00 +0530

    Prototype design pattern code push

---
+++ src/Creational/Prototype.Pattern/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype.Pattern.Models
+{
+    public class Product : IPrototype
+    {
+        private string _name;
+        private double _price;
+        private string _description;
+        private double _rating;
+        private string _category;
+        public Product(string name, double price, string description, double rating, string category) 
+        {
+            this._name = name;
+            this._price = price;
+            this._description = description;
+            this._rating = rating;
+            this._price = price;
+            this._category = category;
+        }
+        public IPrototype Clone() => new Product(_name, _price, _description, _rating, _category);
+
+    }
+}
---
+++ src/Creational/Prototype.Pattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using Prototype.Pattern.Models;
+
+namespace Prototype.Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrototype product = new Product("iPhone pro 16 max", 199999.00, "Apple iphone pro 16 max with 16GB RAM & 2TB Internal storage", 4.6, "MobilePhone");
+
+            IPrototype clonedProduct =  product.Clone();
+
+            Console.WriteLine(product.GetHashCode());
+            Console.WriteLine(clonedProduct.GetHashCode());
+        }
+    }
+}
